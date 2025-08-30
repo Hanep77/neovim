2e8871f9893c4d0a5e7ac2cba6753d75023398f6
hanep77 <yudishan26@gmail.com>
2025-08-30 15:39:23 +0700

    add blade formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,25 +1,26 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					python = { "isort", "black" },
-					rust = { "rustfmt", lsp_format = "fallback" },
-					javascript = { "prettierd", "prettier", stop_after_first = true },
-				},
-				format_on_save = {
-					timout_ms = 500,
-					lsp_format = "fallback",
-				},
-			})
-		end,
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		config = function()
-			require("nvim-ts-autotag").setup()
-		end,
-	},
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          python = { "isort", "black" },
+          rust = { "rustfmt", lsp_format = "fallback" },
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+          blade = { "blade-formatter" }
+        },
+        format_on_save = {
+          timout_ms = 500,
+          lsp_format = "fallback",
+        },
+      })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,44 +1,44 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"neovim/nvim-lspconfig",
-		},
-		opts = {
-			servers = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				},
-				ts_ls = {},
-				emmet_language_server = {},
-				eslint = {},
-				tailwindcss = {},
-				jedi_language_server = {},
-				intelephense = {},
-			},
-		},
-		config = function(_, opts)
-			require("mason").setup()
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+            },
+          },
+        },
+        ts_ls = {},
+        emmet_language_server = {},
+        eslint = {},
+        tailwindcss = {},
+        jedi_language_server = {},
+        intelephense = {},
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup()
 
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "eslint", "ts_ls" },
-			})
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "eslint", "ts_ls" },
+      })
 
-			vim.diagnostic.config({
-				virtual_text = true,
-			})
+      vim.diagnostic.config({
+        virtual_text = true,
+      })
 
-			for server, config in pairs(opts.servers) do
-				vim.lsp.config(server, config)
-				vim.lsp.enable(server)
-			end
-		end,
-	},
+      for server, config in pairs(opts.servers) do
+        vim.lsp.config(server, config)
+        vim.lsp.enable(server)
+      end
+    end,
+  },
 }
