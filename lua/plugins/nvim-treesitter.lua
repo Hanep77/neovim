ed54a7b832a187170594b9dc420f1afa7ead3ad4
hanep77 <yudishan26@gmail.com>
2026-09-04 10:21:30 +0700

    fix(treesitter): enable more syntax highlighting

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,28 +1,42 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
 
-	config = function()
-		require("nvim-treesitter").setup()
+  config = function()
+    require("nvim-treesitter").setup()
 
-		require("nvim-treesitter").install({
-			"html",
-			"javascript",
-			"typescript",
-			"lua",
-		})
+    require("nvim-treesitter").install({
+      "lua",
+      "javascript",
+      "typescript",
+      "tsx",
+      "html",
+      "css",
+      "json",
+      "bash",
+      "markdown",
+      "markdown_inline",
+      "vim",
+      "vimdoc",
+    })
 
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = {
-				"html",
-				"javascript",
-				"typescript",
-				"lua",
-			},
-			callback = function()
-				vim.treesitter.start()
-			end,
-		})
-	end,
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "lua",
+        "javascript",
+        "typescript",
+        "typescriptreact",
+        "html",
+        "css",
+        "json",
+        "bash",
+        "markdown",
+        "vim",
+      },
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+  end,
 }
